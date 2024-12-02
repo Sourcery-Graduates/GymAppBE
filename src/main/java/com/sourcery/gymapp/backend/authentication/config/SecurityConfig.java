4177d2d72d9e93c6c8837fffcd73c889839a96dc
Valentyn Bondarenko <murica.it@gmail.com>
2024-12-02 15:47:24 +0100

    Add actuator

--- src/main/java/com/sourcery/gymapp/backend/authentication/config/SecurityConfig.java
+++ src/main/java/com/sourcery/gymapp/backend/authentication/config/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "api/auth/register"
+                                "api/auth/register",
+                                "/actuator/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
