7ba2b17d5238d6a5494055c92e91ccf305b65893
przemyslawren <przemyslaw.ren@gmail.com>
2025-01-21 14:37:20 +0100

    update to record

--- src/main/java/com/sourcery/gymapp/backend/workout/dto/MuscleSetDto.java
+++ src/main/java/com/sourcery/gymapp/backend/workout/dto/MuscleSetDto.java
@@ -1,16 +1,9 @@
 package com.sourcery.gymapp.backend.workout.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
-import java.util.UUID;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class MuscleSetDto {
-    private List<String> primaryMuscles;
-    private Long numberOfSets;
+public record MuscleSetDto(
+    List<String> primaryMuscles,
+    Long numberOfSets
+) {
 }
