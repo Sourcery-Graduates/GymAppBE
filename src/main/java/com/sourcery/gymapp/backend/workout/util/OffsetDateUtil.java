8d5deabe71a08bbd786174ee1725ef523430434b
przemyslawren <przemyslaw.ren@gmail.com>
2025-01-23 15:38:01 +0100

    refactor DateUtil, add comments

--- src/main/java/com/sourcery/gymapp/backend/workout/util/OffsetDateUtil.java
+++ src/main/java/com/sourcery/gymapp/backend/workout/util/OffsetDateUtil.java
@@ -9,57 +9,69 @@ import java.util.List;
 
 @UtilityClass
 public class OffsetDateUtil {
+
+    /**
+     * Returns the start and end of the week with an offset.
+     * @param offsetWeek the number of weeks to offset from the current week.
+     * @return a list containing the start and end of the week.
+     */
     public List<ZonedDateTime> getWeeklyDateRangeOffset(Integer offsetWeek) {
         ZonedDateTime now = ZonedDateTime.now();
         ZonedDateTime startOfWeek = now.minusWeeks(offsetWeek)
                 .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
                 .withHour(0).withMinute(0).withSecond(0).withNano(0);
 
-        ZonedDateTime endOfWeek = startOfWeek.plusDays(6)
+        ZonedDateTime endOfWeek = startOfWeek.plusDays(7)
                 .withHour(23).withMinute(59).withSecond(59).withNano(999999999);
 
         return List.of(startOfWeek, endOfWeek);
     }
 
-
+    /**
+     * Returns the start of the month with an offset and the end of the current month.
+     * @param offsetStartMonth the number of months to offset from the current month.
+     * @return a list containing the start of the offset month and the end of the current month.
+     */
     public List<ZonedDateTime> getStartOffsetAndEndCurrentMonth(Integer offsetStartMonth) {
-        ZonedDateTime currentDate = ZonedDateTime.now();
-
-        ZonedDateTime startOfTheMonth = OffsetDateUtil.getOffsetStartMonthFromCurrentDate(offsetStartMonth, currentDate);
-        ZonedDateTime endOfTheMonth = OffsetDateUtil.getEndMonthFromCurrentDate(currentDate);
+        ZonedDateTime startOfTheMonth = getOffsetStartMonthFromCurrentDate(offsetStartMonth);
+        ZonedDateTime endOfTheMonth = getEndMonthFromCurrentDate();
 
         return List.of(startOfTheMonth, endOfTheMonth);
     }
 
+    /**
+     * Returns the start and end of the month with an offset.
+     * @param offsetMonth the number of months to offset from the current month.
+     * @return a list containing the start and end of the offset month.
+     */
     public List<ZonedDateTime> getMonthlyDateRangeOffset(Integer offsetMonth) {
-        ZonedDateTime currentDate = ZonedDateTime.now();
-        ZonedDateTime startOfTheMonth = currentDate.withDayOfMonth(1).withHour(0);
-        ZonedDateTime endOfTheMonth = currentDate;
-
-        if (offsetMonth != null) {
-            startOfTheMonth = OffsetDateUtil.getOffsetStartMonthFromCurrentDate(offsetMonth, currentDate);
-
-            endOfTheMonth = OffsetDateUtil.getOffsetEndMonthFromCurrentDate(offsetMonth, currentDate);
-        }
+        ZonedDateTime startOfTheMonth = getOffsetStartMonthFromCurrentDate(offsetMonth);
+        ZonedDateTime endOfTheMonth =  getOffsetEndMonthFromCurrentDate(offsetMonth);
 
         return List.of(startOfTheMonth, endOfTheMonth);
     }
 
-    private static ZonedDateTime getOffsetStartMonthFromCurrentDate(Integer offsetStartMonth,
-                                                             ZonedDateTime currentDate) {
-        return currentDate.minusMonths(offsetStartMonth)
+    private static ZonedDateTime getOffsetStartMonthFromCurrentDate(Integer offsetStartMonth) {
+        ZonedDateTime currentDate = ZonedDateTime.now();
+
+        return currentDate
+                .minusMonths(offsetStartMonth)
                 .with(TemporalAdjusters.firstDayOfMonth())
                 .withHour(0).withMinute(0).withSecond(0).withNano(0);
     }
 
-    private static ZonedDateTime getOffsetEndMonthFromCurrentDate(Integer offsetEndMonth,
-                                                           ZonedDateTime currentDate) {
-        return currentDate.minusMonths(offsetEndMonth)
+    private static ZonedDateTime getOffsetEndMonthFromCurrentDate(Integer offsetEndMonth) {
+        ZonedDateTime currentDate = ZonedDateTime.now();
+
+        return currentDate
+                .minusMonths(offsetEndMonth)
                 .with(TemporalAdjusters.lastDayOfMonth())
                 .withHour(23).withMinute(59).withSecond(59).withNano(999999999);
     }
 
-    private static ZonedDateTime getEndMonthFromCurrentDate(ZonedDateTime currentDate) {
+    private static ZonedDateTime getEndMonthFromCurrentDate() {
+        ZonedDateTime currentDate = ZonedDateTime.now();
+
         return currentDate
                 .with(TemporalAdjusters.lastDayOfMonth())
                 .withHour(23).withMinute(59).withSecond(59).withNano(999999999);
