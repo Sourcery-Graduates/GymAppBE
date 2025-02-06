9af06e1e522dd0ce1049fe8c0a764c2055c993bc
przemyslawren <przemyslaw.ren@gmail.com>
2025-02-06 09:45:27 +0100

    delete unnecessary import

--- src/main/java/com/sourcery/gymapp/backend/workout/controller/ExerciseController.java
+++ src/main/java/com/sourcery/gymapp/backend/workout/controller/ExerciseController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/workout/exercise")
