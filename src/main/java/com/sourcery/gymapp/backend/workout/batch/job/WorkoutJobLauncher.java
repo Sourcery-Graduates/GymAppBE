4674dc6fe19d48074bc49d3c5bc3265141ee8047
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-30 16:52:39 +0200

    update error message

--- src/main/java/com/sourcery/gymapp/backend/workout/batch/job/WorkoutJobLauncher.java
+++ src/main/java/com/sourcery/gymapp/backend/workout/batch/job/WorkoutJobLauncher.java
@@ -41,7 +41,7 @@ public class WorkoutJobLauncher {
         try {
             jobLauncher.run(lastWorkoutRemindUserJob, jobParameters);
         } catch (Exception e) {
-            log.error("Failed to manual run lastWorkoutReminderJob", e);
+            log.error("Failed to manually run lastWorkoutReminderJob", e);
         }
     }
 
