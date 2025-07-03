742adf8785c2fcc8fd1aa9d132a8b6aa10c3b28e
przemyslawren <przemyslaw.ren@gmail.com>
2025-07-03 17:33:59 +0200

    update endpoint mapping

--- src/main/java/com/sourcery/gymapp/backend/authentication/controller/AdminAuthController.java
+++ src/main/java/com/sourcery/gymapp/backend/authentication/controller/AdminAuthController.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Profile("!deployment")
 @RestController
+@RequestMapping("/api/auth-admin")
 @RequiredArgsConstructor
 public class AdminAuthController {
     private final AuthService authService;
